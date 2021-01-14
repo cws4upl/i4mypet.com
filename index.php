ccbcdfb4e320a74f8772084ec0a8c98b3b26f3f2
Katarzyna Parzybut <keparzybut@gmail.com>
2021-01-14 22:20:00 +0100

    register.php added

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo("created");
+$con = mysqli_connect("localhost", "root", "", "social");
+
+if(mysqli_connect_errno())
+{
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+
+?>
+
+<html>
+<head>
+    <title>Welcome to I for my pet</title>
+</head>
+<body>
+My pupy
+</body>
+</html>
